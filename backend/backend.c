9e1a2ab25f7e1db3f1982ecd076333c2b5758a19
Aki-7 <aki.develop8128@gmail.com>
2021-12-29 00:07:30 +0900

    zigen protocols

--- backend/backend.c
+++ backend/backend.c
@@ -1,5 +1,8 @@
 #include <string.h>
 #include <wayland-client.h>
+#include <zigen-client-protocol.h>
+#include <zigen-opengl-client-protocol.h>
+#include <zigen-shell-client-protocol.h>
 #include <zmonitors-util.h>
 
 #include "zmonitors-backend.h"
@@ -8,7 +11,38 @@ struct zms_backend {
   struct wl_display* display;
 
   /* globals */
+  struct zgn_compositor* compositor;
+  struct zgn_seat* seat;
+  struct zgn_shell* shell;
   struct wl_shm* shm;
+  struct zgn_opengl* opengl;
+};
+
+static void
+seat_capabilities(void* data, struct zgn_seat* seat, uint32_t capability)
+{
+  // TODO:
+  zms_log("event not implemented yet: zgn_seat.capabilities\n");
+  Z_UNUSED(data);
+  Z_UNUSED(seat);
+  Z_UNUSED(capability);
+}
+
+static const struct zgn_seat_listener seat_listener = {
+    .capabilities = seat_capabilities,
+};
+
+static void
+shm_format(void* data, struct wl_shm* shm, uint32_t format)
+{
+  // we use only the default formats (0 and 1).
+  Z_UNUSED(data);
+  Z_UNUSED(shm);
+  Z_UNUSED(format);
+}
+
+static const struct wl_shm_listener shm_listener = {
+    .format = shm_format,
 };
 
 static void
@@ -17,9 +51,22 @@ global_registry_handler(void* data, struct wl_registry* registry, uint32_t id,
 {
   struct zms_backend* backend = data;
 
-  if (strcmp(interface, "wl_shm") == 0) {
+  if (strcmp(interface, "zgn_compositor") == 0) {
+    backend->compositor =
+        wl_registry_bind(registry, id, &zgn_compositor_interface, version);
+  } else if (strcmp(interface, "zgn_seat") == 0) {
+    backend->seat =
+        wl_registry_bind(registry, id, &zgn_seat_interface, version);
+    zgn_seat_add_listener(backend->seat, &seat_listener, backend);
+  } else if (strcmp(interface, "zgn_shell") == 0) {
+    backend->shell =
+        wl_registry_bind(registry, id, &zgn_shell_interface, version);
+  } else if (strcmp(interface, "wl_shm") == 0) {
     backend->shm = wl_registry_bind(registry, id, &wl_shm_interface, version);
-    // TODO: add listener
+    wl_shm_add_listener(backend->shm, &shm_listener, backend);
+  } else if (strcmp(interface, "zgn_opengl") == 0) {
+    backend->opengl =
+        wl_registry_bind(registry, id, &zgn_opengl_interface, version);
   }
 }
 
@@ -82,7 +129,9 @@ zms_backend_connect(struct zms_backend* backend, const char* socket)
   wl_display_dispatch(display);
   wl_display_roundtrip(display);
 
-  if (backend->shm == NULL) goto err_globals;
+  if (backend->compositor == NULL || backend->seat == NULL ||
+      backend->shell == NULL || backend->shm == NULL || backend->opengl == NULL)
+    goto err_globals;
 
   return true;
 
